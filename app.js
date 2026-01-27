bcd000335fe4d95eca53ab1711515ec003749e9e
Suraj Jha <114465421+Surajjha8766@users.noreply.github.com>
2026-01-27 22:25:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const app = express();
 app.use(cookieParser()) 
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000", // Replace with your frontend URL
+  // origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: true,
   credentials: true // Allow cookies to be sent
 }));
 app.use(express.json());
